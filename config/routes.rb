ac3e2b475e7d64c2efdeb318fe14c5bba15e374e
Juan Diego Bueno <juandiegobueno@cifpaviles.org>
2022-04-13 17:17:31 +0000

    Nuevos controlador y vista

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'saludo/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/saludo", to: "saludo#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
